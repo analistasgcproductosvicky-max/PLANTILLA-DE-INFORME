5563d4b23b4aabaeb7f4740c9a09a017db7daaea
analistasgcproductosvicky-max <analistasgcproductosvicky@gmail.com>
2026-06-10 11:08:48 -0500

    Add 'David Diaz' to the PERSONAS array

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PERSONAS = [
   'Juan Sebastián Parra','Oscar Bautista','Juan Pablo Rodríguez',
   'Yennireth Villarreal','Gloria Hernández','Valeria Hernández',
   'Cristian Aranda','Ángel Ramírez','Katherine Florez',
-  'María Alejandra Cárdenas','Ely Padilla'
+  'María Alejandra Cárdenas','Ely Padilla','David Diaz'
 ];
 
 const PERSONAS_AREA = {
